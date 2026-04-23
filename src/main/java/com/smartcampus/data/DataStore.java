fd4866cee57bb6766f44900d182b21e652d24186
kishon-uni <keith.20211556@iit.ac.lk>
2026-04-23 22:13:45 +0530

    chore: add helper method to get the relavant ids

--- src/main/java/com/smartcampus/data/DataStore.java
+++ src/main/java/com/smartcampus/data/DataStore.java
@@ -9,51 +9,68 @@ import java.util.List;
 import java.util.Map;
 
 public class DataStore {
+    private static final String ROOM_ID_PREFIX = "R-";
+    private static final String SENSOR_ID_PREFIX = "S-";
+    private static final String SENSOR_READING_ID_PREFIX = "SR-";
+
     public static final Map<String, Room> ROOMS = new HashMap<>();
     public static final Map<String, Sensor> SENSORS = new HashMap<>();
     public static final Map<String, List<SensorReading>> SENSOR_READINGS = new HashMap<>();
 
+    public static String nextRoomId() {
+        return ROOM_ID_PREFIX + (ROOMS.size() + 1);
+    }
+
+    public static String nextSensorId() {
+        return SENSOR_ID_PREFIX + (SENSORS.size() + 1);
+    }
+
+    public static String nextSensorReadingId() {
+        int sensorReadingCount = SENSOR_READINGS.values().stream().mapToInt(List::size).sum();
+        return SENSOR_READING_ID_PREFIX + (sensorReadingCount + 1);
+    }
+
     static {
         Room room1 = new Room();
-        room1.setId("LIB-301");
+        room1.setId("R-1");
         room1.setName("Library Quiet Study");
         room1.setCapacity(40);
         ROOMS.put(room1.getId(), room1);
 
         Room room2 = new Room();
-        room2.setId("ENG-101");
+        room2.setId("R-2");
         room2.setName("Engineering Lab A");
         room2.setCapacity(25);
         ROOMS.put(room2.getId(), room2);
 
         Sensor sensor1 = new Sensor();
-        sensor1.setId("TEMP-001");
+        sensor1.setId("S-1");
         sensor1.setType("Temperature");
         sensor1.setStatus("ACTIVE");
         sensor1.setCurrentValue(21.5);
-        sensor1.setRoomId("LIB-301");
+        sensor1.setRoomId("R-1");
         SENSORS.put(sensor1.getId(), sensor1);
-        ROOMS.get("LIB-301").getSensorIds().add(sensor1.getId());
+        ROOMS.get("R-1").getSensorIds().add(sensor1.getId());
         SENSOR_READINGS.put(sensor1.getId(), new ArrayList<>());
 
         Sensor sensor2 = new Sensor();
-        sensor2.setId("CO2-001");
+        sensor2.setId("S-2");
         sensor2.setType("CO2");
         sensor2.setStatus("ACTIVE");
         sensor2.setCurrentValue(450.0);
-        sensor2.setRoomId("ENG-101");
+        sensor2.setRoomId("R-2");
         SENSORS.put(sensor2.getId(), sensor2);
-        ROOMS.get("ENG-101").getSensorIds().add(sensor2.getId());
+        ROOMS.get("R-2").getSensorIds().add(sensor2.getId());
         SENSOR_READINGS.put(sensor2.getId(), new ArrayList<>());
 
         Sensor sensor3 = new Sensor();
-        sensor3.setId("OCC-001");
+        sensor3.setId("S-3");
         sensor3.setType("Occupancy");
         sensor3.setStatus("MAINTENANCE");
         sensor3.setCurrentValue(0.0);
-        sensor3.setRoomId("LIB-301");
+        sensor3.setRoomId("R-1");
         SENSORS.put(sensor3.getId(), sensor3);
-        ROOMS.get("LIB-301").getSensorIds().add(sensor3.getId());
+        ROOMS.get("R-1").getSensorIds().add(sensor3.getId());
         SENSOR_READINGS.put(sensor3.getId(), new ArrayList<>());
     }
 }
